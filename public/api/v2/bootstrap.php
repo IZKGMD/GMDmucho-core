bcd27a4288055688a2aa6e5dcf7d0f86e95c7b9d
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-20 21:31:00 +0700

    fix: make API v2 work on shared hosting paths

--- public/api/v2/bootstrap.php
+++ public/api/v2/bootstrap.php
@@ -10,7 +10,7 @@ use MuchoCore\Security\ClientIp;
 
 require_once dirname(__DIR__, 3) . '/vendor/autoload.php';
 
-const MUCHO_V2_ROOT = '/var/www/mucho-core';
+const MUCHO_V2_ROOT = __DIR__ . '/../../..';
 const MUCHO_V2_VERSION = '2.2';
 
 function muchoV2Env(): array
