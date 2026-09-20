2c722a1c09dbde20bf4704e81e1dd570d9edd91d
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-20 21:40:22 +0700

    fix: wire authenticator into level service

--- src/Level/LevelService.php
+++ src/Level/LevelService.php
@@ -14,6 +14,7 @@ final readonly class LevelService
     public function __construct(
         private LevelRepository $levels,
         private GdLevelListEncoder $encoder,
+        private AccountAuthenticator $auth
     ) {
     }
 
