6410a96ebb335b41d0f94cdf3060edfae2a700ca
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-23 16:24:22 +0700

    Fix custom song protocol disabled field

--- src/Protocol/GdSongEncoder.php
+++ src/Protocol/GdSongEncoder.php
@@ -9,15 +9,16 @@ final class GdSongEncoder
     public function encode(array $song): string
     {
         $data = [
-            1, $song['id'],
-            2, $song['name'],
-            3, $song['author_id'],
-            4, $song['author_name'],
-            5, round((float)$song['size'], 2),
-            6, $song['youtube_video_id'] ?? '',
-            10, $song['download_url'],
-            7, $song['youtube_channel_id'] ?? '',
-            8, (int)($song['is_verified'] ?? 0)
+            1, (int)$song['id'],
+            2, ProtocolText::username((string)($song['name'] ?? '')),
+            3, (int)($song['author_id'] ?? 0),
+            4, ProtocolText::username((string)($song['author_name'] ?? '')),
+            5, round((float)($song['size'] ?? 0), 2),
+            6, (string)($song['youtube_video_id'] ?? ''),
+            10, (string)($song['download_url'] ?? ''),
+            7, (string)($song['youtube_channel_id'] ?? ''),
+            // GD field 8 is "disabled", not "verified".
+            8, (int)($song['is_disabled'] ?? 0),
         ];
 
         return implode('~|~', $data);
