21b72806e6ce9abbaad2c22fb1d023a0c357599b
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-23 19:38:40 +0700

    Fix standalone Turnstile test

--- tests/turnstile.php
+++ tests/turnstile.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use MuchoCore\Security\Turnstile;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/src/Security/Turnstile.php';
 
 $previousSite = getenv('TURNSTILE_SITEKEY');
 $previousSecret = getenv('TURNSTILE_SECRET');
