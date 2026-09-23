d4e2597670e87e1c4e13f19b44bc548557bc463b
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-23 20:26:23 +0700

    test: lock down Geometry Dash 2.2 protocol fixes

--- tests/level-protocol-guards.php
+++ tests/level-protocol-guards.php
@@ -30,8 +30,53 @@ $checks = [
     ],
     [
         __DIR__ . '/../src/Level/LevelRepository.php',
-        "case 27:",
+        'case 27:',
         'suggested level discovery type 27',
+    ],
+    [
+        __DIR__ . '/../database/migrations/018_profile_protocol_state.php',
+        "demon_info",
+        '2.2 profile state migration',
+    ],
+    [
+        __DIR__ . '/../src/User/UserService.php',
+        "'demon_info' =>",
+        '2.2 demon info persistence',
+    ],
+    [
+        __DIR__ . '/../src/User/UserService.php',
+        "'star_info' =>",
+        '2.2 star info persistence',
+    ],
+    [
+        __DIR__ . '/../src/User/UserService.php',
+        "'platformer_info' =>",
+        '2.2 platformer info persistence',
+    ],
+    [
+        __DIR__ . '/../src/User/UserService.php',
+        "'glow' =>",
+        '2.2 glow persistence',
+    ],
+    [
+        __DIR__ . '/../src/User/UserService.php',
+        "'special' =>",
+        '2.2 special persistence',
+    ],
+    [
+        __DIR__ . '/../src/User/UserController.php',
+        '$gjp=$request->gdCredential();',
+        '2.2 version-aware score credentials',
+    ],
+    [
+        __DIR__ . '/../src/Moderation/ModerationController.php',
+        'return $request->gdCredential();',
+        '2.2 version-aware moderation credentials',
+    ],
+    [
+        __DIR__ . '/../src/Score/PlatformerScoreController.php',
+        "':15:'.(int)$row['color3'].",
+        '2.2 platformer color3 field',
     ],
 ];
 
@@ -39,14 +84,11 @@ foreach ($checks as [$file, $needle, $name]) {
     $contents = file_get_contents($file);
 
     if ($contents === false || !str_contains($contents, $needle)) {
-        fwrite(STDERR, "FAIL {$name}
-");
+        fwrite(STDERR, "FAIL {$name}\n");
         exit(1);
     }
 
-    echo "PASS {$name}
-";
+    echo "PASS {$name}\n";
 }
 
-echo "MUCHOCORE_LEVEL_PROTOCOL_GUARDS_OK
-";
+echo "MUCHOCORE_LEVEL_PROTOCOL_GUARDS_OK\n";
