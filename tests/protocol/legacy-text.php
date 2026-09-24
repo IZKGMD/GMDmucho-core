de15a5e257354be439f70d1774ac523e5b32b5c5
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-24 17:53:01 +0700

    Fix test paths after test suite reorganization

--- tests/protocol/legacy-text.php
+++ tests/protocol/legacy-text.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../src/Protocol/GdLegacyText.php';
+require __DIR__ . '/../../src/Protocol/GdLegacyText.php';
 
 use MuchoCore\Protocol\GdLegacyText;
 
