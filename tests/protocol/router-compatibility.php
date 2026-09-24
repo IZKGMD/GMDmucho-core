907fa26459050cff25e4b2ac2ac15272a302d872
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-24 17:53:22 +0700

    Fix moved test paths

--- tests/protocol/router-compatibility.php
+++ tests/protocol/router-compatibility.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../src/Http/Request.php';
-require __DIR__ . '/../src/Http/Response.php';
-require __DIR__ . '/../src/Routing/Router.php';
+require __DIR__ . '/../../src/Http/Request.php';
+require __DIR__ . '/../../src/Http/Response.php';
+require __DIR__ . '/../../src/Routing/Router.php';
 
 use MuchoCore\Http\Request;
 use MuchoCore\Http\Response;
