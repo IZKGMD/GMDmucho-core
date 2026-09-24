d1c81c01ce518b8ba6bc937b36807d433a80641b
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-24 21:39:52 +0700

    Infer GD 1.9 from versionless comment endpoints

--- src/Compatibility/ClientVersion.php
+++ src/Compatibility/ClientVersion.php
@@ -26,7 +26,7 @@ final readonly class ClientVersion
         if (
             $gameVersion === 0 &&
             preg_match(
-                '#/(?:getgjcomments19|uploadgjcomment19|deletegjcomment19|updategjlevel19)(?:\\.php)?$#i',
+                '#/(?:getgjcomments(?:19)?|uploadgjcomment(?:19)?|deletegjcomment(?:19)?|updategjlevel19)(?:\\.php)?$#i',
                 (string)$request->path
             ) === 1
         ) {
