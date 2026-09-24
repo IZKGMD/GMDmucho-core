e9437cb0c642d06dfcbada37f95beeb24a1ff6fd
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-24 18:10:12 +0700

    Fix 2.1 wire test protocol text dependency

--- tests/protocol/protocol-21-wire.php
+++ tests/protocol/protocol-21-wire.php
@@ -9,6 +9,7 @@ require __DIR__ . '/../../src/Protocol/GdLevelDownloadEncoder.php';
 require __DIR__ . '/../../src/Protocol/GdLevelListEncoder.php';
 require __DIR__ . '/../../src/Protocol/GdUserEncoder.php';
 require __DIR__ . '/../../src/Protocol/GdXor.php';
+require __DIR__ . '/../../src/Protocol/ProtocolText.php';
 
 use MuchoCore\Compatibility\ClientVersion;
 use MuchoCore\Protocol\GdCommentEncoder;
