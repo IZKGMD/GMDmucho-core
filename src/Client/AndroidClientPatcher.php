ef33a0adb63e6058312f2a9a535e9782cf5a5d3c
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-25 23:18:57 +0700

    fix: pass zipalign page alignment flag correctly

--- src/Client/AndroidClientPatcher.php
+++ src/Client/AndroidClientPatcher.php
@@ -154,7 +154,7 @@ final class AndroidClientPatcher
         }
 
         self::runTool(
-            ['zipalign', '-p', '16', '-f', '4', $rebuiltPath, $alignedPath],
+            ['zipalign', '-p', '-f', '4', $rebuiltPath, $alignedPath],
             'zipalign'
         );
 
