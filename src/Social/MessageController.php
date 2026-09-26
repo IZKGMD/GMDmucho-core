22f1e434557574ffa232ee079dcd3b76b69ab1c4
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-26 22:55:52 +0700

    refactor: simplify message controller

--- src/Social/MessageController.php
+++ src/Social/MessageController.php
@@ -1,82 +1,62 @@
 <?php
+
 declare(strict_types=1);
 
 namespace MuchoCore\Social;
 
+use MuchoCore\Http\LegacyEndpoint;
 use MuchoCore\Http\Request;
 use MuchoCore\Http\Response;
-use Throwable;
 
 final readonly class MessageController
 {
     public function __construct(private MessageService $service) {}
 
-    private function gjp(Request $r): string
-    {
-        return $r->gdCredential();
-    }
-
     public function getMessages(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->getMessages(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    max(0, $r->postInt('page')),
-                    $r->postInt('getSent') === 1
-                )
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->getMessages(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                max(0,$r->postInt('page')),
+                $r->postInt('getSent')===1
+            )
+        );
     }
 
     public function readMessage(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->readMessage(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('messageID')
-                )
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->readMessage(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('messageID')
+            )
+        );
     }
 
     public function sendMessage(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->sendMessage(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('toAccountID'),
-                    $r->postString('subject'),
-                    $r->postString('body')
-                ) ? '1' : '-1'
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->sendMessage(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('toAccountID'),
+                $r->postString('subject'),
+                $r->postString('body')
+            ) ? '1' : '-1'
+        );
     }
 
     public function deleteMessage(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->deleteMessage(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('messageID'),
-                    $r->postInt('isSender') === 1
-                ) ? '1' : '-1'
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->deleteMessage(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('messageID'),
+                $r->postInt('isSender')===1
+            ) ? '1' : '-1'
+        );
     }
 }
