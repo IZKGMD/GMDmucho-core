f4b6dabd840326935660bc9010b545e0347d37c4
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-26 22:55:32 +0700

    refactor: simplify song controller

--- src/Music/SongController.php
+++ src/Music/SongController.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace MuchoCore\Music;
 
+use MuchoCore\Http\LegacyEndpoint;
 use MuchoCore\Http\Request;
 use MuchoCore\Http\Response;
-use Throwable;
 
 final readonly class SongController
 {
@@ -16,16 +16,14 @@ final readonly class SongController
 
     public function info(Request $request): Response
     {
-        $songId = $request->postInt('songID');
+        $songId=$request->postInt('songID');
 
-        if ($songId <= 0) {
+        if ($songId<=0) {
             return Response::text('-1');
         }
 
-        try {
-            return Response::text($this->service->getSongInfo($songId));
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(
+            fn(): string => $this->service->getSongInfo($songId)
+        );
     }
 }
