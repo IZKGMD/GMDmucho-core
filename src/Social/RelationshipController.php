e1043c478b344d48437029ac5f77be277529978e
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-26 22:55:54 +0700

    refactor: simplify relationship controller

--- src/Social/RelationshipController.php
+++ src/Social/RelationshipController.php
@@ -1,156 +1,116 @@
 <?php
+
 declare(strict_types=1);
 
 namespace MuchoCore\Social;
 
+use MuchoCore\Http\LegacyEndpoint;
 use MuchoCore\Http\Request;
 use MuchoCore\Http\Response;
-use Throwable;
 
 final readonly class RelationshipController
 {
     public function __construct(private RelationshipService $service) {}
 
-    private function gjp(Request $r): string
-    {
-        return $r->gdCredential();
-    }
-
     public function send(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->send(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('toAccountID'),
-                    $r->postString('comment')
-                ) ? '1' : '-1'
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->send(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('toAccountID'),
+                $r->postString('comment')
+            ) ? '1' : '-1'
+        );
     }
 
     public function get(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->get(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    max(0, $r->postInt('page')),
-                    $r->postInt('getSent') === 1
-                )
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->get(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                max(0,$r->postInt('page')),
+                $r->postInt('getSent')===1
+            )
+        );
     }
 
     public function read(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->read(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('requestID')
-                ) ? '1' : '-1'
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->read(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('requestID')
+            ) ? '1' : '-1'
+        );
     }
 
     public function accept(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->accept(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('requestID')
-                ) ? '1' : '-1'
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->accept(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('requestID')
+            ) ? '1' : '-1'
+        );
     }
 
     public function delete(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->delete(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('targetAccountID'),
-                    $r->postInt('isSender') === 1
-                ) ? '1' : '-1'
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->delete(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('targetAccountID'),
+                $r->postInt('isSender')===1
+            ) ? '1' : '-1'
+        );
     }
 
     public function remove(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->remove(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('targetAccountID')
-                ) ? '1' : '-1'
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->remove(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('targetAccountID')
+            ) ? '1' : '-1'
+        );
     }
 
     public function block(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->block(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('targetAccountID')
-                ) ? '1' : '-1'
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->block(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('targetAccountID')
+            ) ? '1' : '-1'
+        );
     }
 
     public function unblock(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->unblock(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('targetAccountID')
-                ) ? '1' : '-1'
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->unblock(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('targetAccountID')
+            ) ? '1' : '-1'
+        );
     }
 
     public function userList(Request $r): Response
     {
-        try {
-            return Response::text(
-                $this->service->userList(
-                    $r->postInt('accountID'),
-                    $this->gjp($r),
-                    $r->postInt('type')
-                )
-            );
-        } catch (Throwable) {
-            return Response::text('-1');
-        }
+        return LegacyEndpoint::text(fn(): string =>
+            $this->service->userList(
+                $r->postInt('accountID'),
+                $r->gdCredential(),
+                $r->postInt('type')
+            )
+        );
     }
 }
