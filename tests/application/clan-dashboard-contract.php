f2d3990af875513470468832bdf869f2ec307bdf
IZK <112145870+IZKGMD@users.noreply.github.com>
2026-09-27 00:07:50 +0700

    test: fix literal clan permissions assertion

--- tests/application/clan-dashboard-contract.php
+++ tests/application/clan-dashboard-contract.php
@@ -158,7 +158,7 @@ assertClanDashboardContract(
 
 assertClanDashboardContract(
     str_contains($dashboard, "myClan['permissions']") &&
-    str_contains($dashboard, "str_replace('_', ' ', $permission)"),
+    str_contains($dashboard, 'str_replace(\'_\', \' \', $permission)'),
     'dashboard exposes explicit clan permissions'
 );
 
